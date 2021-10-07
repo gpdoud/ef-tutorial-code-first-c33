53fac4ba9da5a660276db15b83c614905f3841e0
Greg Doud <gpdoud@gmail.com>
2021-10-07 16:29:38 -0400

    Add project files.

---
+++ ef-tutorial-code-first/Program.cs
@@ -0,0 +1,21 @@
+﻿using SalesDbLib.Controllers;
+
+using System;
+using System.Threading.Tasks;
+
+namespace ef_tutorial_code_first
+{
+	class Program
+	{
+		async static Task Main(string[] args)
+		{
+			var ordCtrl = new OrdersController();
+			var orders = await ordCtrl.GetAll();
+			orders.ForEach(o => Console.WriteLine($"{o.Description} | {o.Customer.Name}"));
+
+			var custCtrl = new CustomersController();
+			var customers = await custCtrl.GetAll();
+			customers.ForEach(c => Console.WriteLine(c.Name));
+		}
+	}
+}
